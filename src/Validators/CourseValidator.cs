59af453022c12b87db6f55c9c6308285fe731fa1
Mendi <meny2800@gmail.com>
2025-11-07 00:38:08 +0200

    Add services, repositories, and validators

---
+++ src/Validators/CourseValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using eTeacherProject.Models.Entities;
+
+namespace eTeacherProject.Validators;
+
+public class CourseValidator : AbstractValidator<Course>
+{
+    public CourseValidator()
+    {
+        RuleFor(c => c.Title).NotEmpty().MaximumLength(100);
+        RuleFor(c => c.Description).NotEmpty().MaximumLength(500);
+        RuleFor(c => c.Lecturer).NotEmpty().MaximumLength(100);
+    }
+}
---
+++ src/Validators/StudentValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using eTeacherProject.Models.Entities;
+
+namespace eTeacherProject.Validators;
+
+public class StudentValidator : AbstractValidator<Student>
+{
+    public StudentValidator()
+    {
+        RuleFor(s => s.Name).NotNull().NotEmpty().MinimumLength(2);
+        RuleFor(s => s.Telephone).NotEmpty().MaximumLength(30);
+        RuleFor(s => s.Address).NotEmpty().MaximumLength(200);
+    }
+}
