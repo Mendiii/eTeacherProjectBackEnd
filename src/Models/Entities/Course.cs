2d4b8f90d4efc03e3ec47ef247bc57081336b506
Mendi <meny2800@gmail.com>
2025-11-07 15:37:08 +0200

    New folder structure

--- src/Models/Entities/Course.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace eTeacherProject.Models.Entities;
-
-public class Course
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public string Lecturer { get; set; } = string.Empty;
-}
