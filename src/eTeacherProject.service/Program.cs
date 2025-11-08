3cfd456f9285973151f67fe089b84d9172a89524
Mendi <meny2800@gmail.com>
2025-11-08 21:55:14 +0200

    add CORS support

--- src/eTeacherProject.service/Program.cs
+++ src/eTeacherProject.service/Program.cs
@@ -9,6 +9,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy => policy
+            .WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
 bool useSql = builder.Configuration.GetValue<bool>("UseSqlRepository");
 builder.Services.AddAppServices(builder.Configuration, useSql);
 
@@ -18,6 +27,8 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowReactApp");
 
 app.UseAuthorization();
 
